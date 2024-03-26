3727cd1b450af94c72ba658c6cf3edd145befd67
Moab Alves <moab.alves@dcx.ufpb.br>
2024-03-26 09:36:59 -0300

    Criado a classe funcionario e estabelecido o relacionamento entre funcionario e cago

--- src/main/java/com/mballem/curso/boot/domain/Cargo.java
+++ src/main/java/com/mballem/curso/boot/domain/Cargo.java
@@ -1,5 +1,7 @@
 package com.mballem.curso.boot.domain;
 
+import java.util.List;
+
 import jakarta.persistence.*;
 
 @SuppressWarnings("serial")
@@ -9,6 +11,9 @@ public class Cargo extends AbstractEntity<Long>{
 
 	@Column(name = "nome", nullable = false, unique = true, length = 60)
 	private String nome;
+	
+	@OneToMany(mappedBy = "cargo")
+	private List<Funcionario> funcionarios;
 	
 	@ManyToOne
 	@JoinColumn(name = "id_departamento_fk")
