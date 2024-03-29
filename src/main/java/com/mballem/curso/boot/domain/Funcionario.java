ae5c17a73d924a2e07fb995e2dce247574757462
Moab Alves <moab.alves@dcx.ufpb.br>
2024-03-29 10:01:10 -0300

    Criado a função para cadastrar funcionários

--- src/main/java/com/mballem/curso/boot/domain/Funcionario.java
+++ src/main/java/com/mballem/curso/boot/domain/Funcionario.java
@@ -3,29 +3,37 @@ package com.mballem.curso.boot.domain;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.format.annotation.NumberFormat.Style;
+
 import jakarta.persistence.*;
 
 @SuppressWarnings("serial")
 @Entity
 @Table(name = "FUNCIONARIOS")
-public class Funcionario extends AbstractEntity<Long>{
+public class Funcionario extends AbstractEntity<Long> {
 
 	@Column(nullable = false, unique = true)
 	private String nome;
-	
+
+	@NumberFormat(style = Style.CURRENCY, pattern = "#,##0.00")
 	@Column(nullable = false, columnDefinition = "DECIMAL(7,2) DEFAULT 0.00")
 	private BigDecimal salario;
-	
+
+	@DateTimeFormat(iso = ISO.DATE)
 	@Column(name = "data_entrada", nullable = false, columnDefinition = "DATE")
 	private LocalDate dataEntrada;
-	
+
+	@DateTimeFormat(iso = ISO.DATE)
 	@Column(name = "data_saida", columnDefinition = "DATE")
 	private LocalDate dataSaida;
-	
+
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "endereco_id_fk")
 	private Endereco endereco;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "cargo_id_fk")
 	private Cargo cargo;
@@ -77,5 +85,4 @@ public class Funcionario extends AbstractEntity<Long>{
 	public void setCargo(Cargo cargo) {
 		this.cargo = cargo;
 	}
-	
 }
